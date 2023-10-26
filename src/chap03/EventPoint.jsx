6c5533d599084d87ba0184abd17f914d04319626
misaku54 <eisei0504@outlook.jp>
2023-10-26 10:14:09 +0900

    [add]

---
+++ src/chap03/EventPoint.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './EventPoint.css';
+
+export default function EventPoint(){
+  const [screen, setScreen] = useState({ x: 0, y: 0});
+  const [page, setPage]     = useState({ x: 0, y: 0});
+  const [client, setClient] = useState({ x: 0, y: 0});
+  const [offset, setOffset] = useState({ x: 0, y: 0});
+
+  const handleMousemove = e => {
+    setScreen({ x: e.screenX, y: e.screenY });
+    setPage({ x: e.pageX, y: e.pageY});
+    setClient({ x: e.clientX, y: e.clientY});
+    setOffset({ x: e.offsetX})
+  }
+}
