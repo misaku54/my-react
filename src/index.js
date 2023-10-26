8c6dac919eff43b42bf4d1fd668914968da734be
misaku54 <eisei0504@outlook.jp>
2023-10-26 20:01:50 +0900

    [add]3-4-14

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import EventMouse from './chap03/EventMouse';
+import EventKey from './chap03/EventKey';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Number オブジェクトのインスタンスメソッドである toLocaleString は、数値を指定したロケールとオプションに応じた形式で文字列に変換した値を取得することができます
 root.render(
-  <EventMouse
-    alt="ロゴ画像"
-    beforeSrc="https://www.web-deli.com/image/linkbanner_l.gif"
-    afterSrc="https://www.web-deli.com/image/home_chara.gif"
-    />
+  <EventKey />
 );
 
 
